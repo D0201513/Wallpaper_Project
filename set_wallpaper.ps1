6c3ab69080396875a6d30e137d99ce08e95f2928
D0201513 <aravind_slcs_intern2@aravind.org>
2025-06-21 11:21:20 +0530

    Update set_wallpaper.ps1

--- set_wallpaper.ps1
+++ set_wallpaper.ps1
@@ -1,23 +1,12 @@
-$imgPath = "C:\Users\Public\Pictures\wallpaper.png"
-
 $code = @"
 using System;
 using System.Runtime.InteropServices;
-
-public static class NativeMethods {
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+public class Wallpaper {
+  [DllImport("user32.dll", SetLastError = true)]
+  public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 }
 "@
+Add-Type $code
 
-Add-Type -TypeDefinition $code
-[NativeMethods]::SystemParametersInfo(20, 0, $imgPath, 3)
-
-$taskName = "SetWallpaperTask"
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -Command `"Add-Type -TypeDefinition '$code'; [NativeMethods]::SystemParametersInfo(20, 0, '$imgPath', 3)`""
-$principal = New-ScheduledTaskPrincipal -UserId "$env:USERNAME" -LogonType Interactive -RunLevel Highest
-$task = New-ScheduledTask -Action $action -Principal $principal
-Register-ScheduledTask -TaskName $taskName -InputObject $task -Force
-Start-ScheduledTask -TaskName $taskName
-Start-Sleep -Seconds 5
-Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+$imgPath = "C:\Users\Public\Pictures\wallpaper.png"
+[Wallpaper]::SystemParametersInfo(20, 0, $imgPath, 3)
